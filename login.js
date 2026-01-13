d44173821129d741a642251915221da60d284584
zeynepsilao <orsasila35@gmail.com>
2026-01-14 02:03:15 +0300

    Update login.js

--- login.js
+++ login.js
@@ -1,14 +1,9 @@
-// login.js - Sadece giriş sayfasını yönetir
-
 document.getElementById("loginForm").addEventListener("submit", function(event){
-    event.preventDefault(); // Sayfanın yenilenmesini engelle
+    event.preventDefault();
 
-    // 1. Tarayıcı hafızasına 'Giriş Yapıldı' notunu düş
     localStorage.setItem('isLoggedIn', 'true');
 
-    // 2. Kullanıcıya bilgi ver
     alert("Login Successful! Redirecting to Home...");
 
-    // 3. Ana sayfaya geri gönder
     window.location.href = "index.html";
 });
