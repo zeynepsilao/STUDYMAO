cd805f13a0cb043de5d4034ce2338825237f6815
zeynepsilao <orsasila35@gmail.com>
2026-01-14 02:03:40 +0300

    Update signup.js

--- signup.js
+++ signup.js
@@ -1,17 +1,7 @@
-// signup.js - Kayıt işlemlerini yönetir
-
 document.getElementById("signupForm").addEventListener("submit", function(event){
-    event.preventDefault(); // Sayfa yenilenmesin
-
-    // 1. Sanki veritabanına kaydetmişiz gibi davranalım
-    // (Burada ileride gerçek kayıt kodları olur)
-
-    // 2. Otomatik giriş yapılmış sayalım
+    event.preventDefault();
+    // Otomatik giriş şeklinde ayarladım
     localStorage.setItem('isLoggedIn', 'true');
-
-    // 3. Kullanıcıya haber verelim
     alert("Account created successfully! Welcome to StudyMao. 🚀");
-
-    // 4. Ana sayfaya (veya Study Zone'a) yönlendirelim
     window.location.href = "study-zone.html";
 });
